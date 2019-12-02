7d855fb0282756a5b71a5d7d9e98d12f64005410
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2019-12-02 16:50:17 +0700

    first commit

---
+++ features/movies/src/main/java/com/godohdev/movies/Constants.kt
@@ -0,0 +1,15 @@
+package com.godohdev.movies
+
+
+/**
+ *
+ * Created by Wahyu Permadi on 2019-11-29.
+ * Android Engineer
+ *
+ **/
+
+object Constants {
+    fun getString() : String{
+        return "Hello Wahyu"
+    }
+}
