6c63c739f1838de32b104209d181f40a4129d3c8
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2019-12-11 12:18:35 +0700

    add(buildSrc): add dependency holder at buildSrc using kotlin dsl

---
+++ buildSrc/src/main/java/Depedencies.kt
@@ -0,0 +1,21 @@
+/**
+ *
+ * Created by Wahyu Permadi on 2019-12-11.
+ * Android Engineer
+ *
+ **/
+
+object ApplicationId {
+    const val appId = "com.godohdev.modular_architecture"
+}
+
+object Release {
+    const val versionCode = 1
+    const val versionName = "1.0"
+}
+
+object Versions {
+    const val minSdk = 19
+    const val compiledSdk = 29
+    const val targetSdk = 29
+}
