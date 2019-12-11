6ff2feb71049e5e312a75f95185ce8fe24ac80d2
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2019-12-11 12:58:34 +0700

    feature(TvShow Module): add new feature module TvShow

--- app/src/main/java/com/godohdev/modular_architecture/MainActivity.kt
+++ app/src/main/java/com/godohdev/modular_architecture/MainActivity.kt
@@ -2,15 +2,11 @@ package com.godohdev.modular_architecture
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.util.Log
-import com.godohdev.movies.Constants
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-
-        Log.d("DATA_GUE","DATA "+Constants.getString())
     }
 }
