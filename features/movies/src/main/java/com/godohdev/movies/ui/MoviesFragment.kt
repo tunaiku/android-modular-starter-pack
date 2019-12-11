e0d484bfac4c1896742f3898c093e453beea8255
Wahyu Permadi <wahyu.permadi1725@gmail.com>
2019-12-11 12:58:34 +0700

    feature(TvShow Module): add new feature module TvShow

---
+++ features/movies/src/main/java/com/godohdev/movies/ui/MoviesFragment.kt
@@ -0,0 +1,29 @@
+package com.godohdev.movies.ui
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+
+/**
+ *
+ * Created by Wahyu Permadi on 2019-12-11.
+ * Android Engineer
+ *
+ **/
+
+class MoviesFragment : Fragment(){
+
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return super.onCreateView(inflater, container, savedInstanceState)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+    }
+}
